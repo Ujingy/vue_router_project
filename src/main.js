407eaa673e9a20ce1c0aac1e1c244fe34cd5f951
Ujingy <88533550+Ujingy@users.noreply.github.com>
2022-05-16 16:27:45 +0900

    new branch commit
    연습 프로젝트

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
+import mixins from "./mixins";
 
 Vue.config.productionTip = false;
+Vue.mixin(mixins);
 
 new Vue({
   router,
---
+++ src/store/modules/user.js
@@ -0,0 +1,58 @@
+export default {
+  namespaced: true,
+
+  state: {
+    token: sessionStorage.getItem("access-token"),
+    user: {
+      id: "",
+      name: "",
+    },
+  },
+
+  getters: {
+    id(state) {
+      return state.user.id;
+    },
+
+    name(state) {
+      return state.user.name;
+    },
+
+    token(state) {
+      return state.token;
+    },
+
+    hasToken(state) {
+      return !!state.token;
+    },
+  },
+
+  mutations: {
+    setName(state, name) {
+      state.user.name = name;
+    },
+
+    setToken(state, token) {
+      state.token = token;
+      sessionStorage.setItem("access-token", token);
+    },
+
+    setId(state, id) {
+      state.user.id = id;
+    },
+  },
+
+  actions: {
+    setName({ commit }, name) {
+      commit("setName", name);
+    },
+
+    setId({ commit }, id) {
+      commit("setId", id);
+    },
+
+    setToken({ commit }, token) {
+      commit("setToken", token);
+    },
+  },
+};
